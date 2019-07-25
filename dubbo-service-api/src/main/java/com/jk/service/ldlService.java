98bfbb415cd3d1e5c87fb5765177ce3b63b634fe
liudl <9196@qq.com>
2019-07-26 07:56:26 +0800

    20:00 ldl第4次提交代码api

--- dubbo-service-api/src/main/java/com/jk/service/ldlService.java
+++ dubbo-service-api/src/main/java/com/jk/service/ldlService.java
@@ -1,14 +1,14 @@
 package com.jk.service;
 
 
-import com.jk.pojo.ldl_commodity_manage;
-import com.jk.pojo.ldl_goods_classify;
+import com.jk.pojo.*;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 
 public interface ldlService {
-    HashMap<String, Object> queryclassify(Integer page, Integer rows);
 
 
     HashMap<String, Object> querycommodity(Integer page, Integer rows, ldl_commodity_manage commodity);
@@ -20,4 +20,30 @@ public interface ldlService {
     List<ldl_goods_classify> findArea(Integer id);
 
     void saveclassify(ldl_goods_classify goods);
+
+
+    List<ldl_goods_classify> tree();
+
+    HashMap<String, Object> findBrand(BrandHxyBean brandHxyBean);
+
+    //----------------------------------------------------------
+    HashMap<String, Object> findLabel(LabelHxyBean labelHxyBean);
+
+    void saveLabel(LabelHxyBean labelHxyBean);
+
+    void deleteLabel(Integer[] ids);
+
+    LabelHxyBean findByIdInfo(Integer id);
+
+    void updateLabel(LabelHxyBean labelHxyBean);
+//----------导出导入
+    void daochu(Integer[] ids);
+
+    void joinche(Integer id);
+
+    HashMap<String, Object> queryche(Integer page, Integer rows, ldl_commodity_manage commodity);
+
+    void updateOne(Integer id);
+
+    HashMap<String, Object> findRepertory(InventoryHxyBean inventoryHxyBean);
 }
