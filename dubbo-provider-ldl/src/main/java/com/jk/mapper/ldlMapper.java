b0bbe17c9d25df8e9b6a0d8be6fa85c1a42247d6
liudl <9196@qq.com>
2019-07-26 07:55:20 +0800

    20:00 ldl第3次提交代码provider-ldl

--- dubbo-provider-ldl/src/main/java/com/jk/mapper/ldlMapper.java
+++ dubbo-provider-ldl/src/main/java/com/jk/mapper/ldlMapper.java
@@ -1,20 +1,13 @@
 package com.jk.mapper;
 
-import com.jk.pojo.ldl_commodity_manage;
-import com.jk.pojo.ldl_goods_classify;
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
+import com.jk.pojo.*;
+import org.apache.ibatis.annotations.*;
 
 import java.util.HashMap;
 import java.util.List;
 
 public interface ldlMapper {
 
-    int findOrderCount(HashMap<String, Object> hashMap);
-
-    List<ldl_goods_classify> findOrderList(@Param("page") int i, @Param("rows") Integer rows);
 
 
     int findUserTotal(@Param("commodity")ldl_commodity_manage commodity);
@@ -26,9 +19,50 @@ public interface ldlMapper {
     @Delete("delete from ldl_goods_classify where id=#{value}")
     void deleteclassifylist(Integer id);
 
-    @Select("select * from ldl_goods_classify where pid=#{value}")
+    @Select("select id,text from ldl_goods_classify where pid=#{value}")
     List<ldl_goods_classify> findArea(Integer id);
 
     @Insert("insert into ldl_goods_classify (name,generalstore,ownstores,pid)")
     void saveclassify(ldl_goods_classify goods);
+
+
+    @Select("select  * from ldl_goods_classify order by id")
+    List<ldl_goods_classify> tree();
+
+
+    int countBrand(@Param("brandHxyBean")BrandHxyBean brandHxyBean);
+
+    List<BrandHxyBean> findBrand(@Param("start")int start, @Param("rows") Integer rows, @Param("brandHxyBean") BrandHxyBean brandHxyBean);
+
+    //---------------------------------------------------------------------
+
+    int countLabel(@Param("labelHxyBean") LabelHxyBean labelHxyBean);
+
+    List<LabelHxyBean> findLabel(@Param("start")int start, @Param("rows")Integer rows, @Param("labelHxyBean")LabelHxyBean labelHxyBean);
+
+    @Insert("insert into t_label(name,sort,img,createTime,remark)values(#{name},#{sort},#{img},SYSDATE(),#{remark})")
+    void saveLabel(LabelHxyBean labelHxyBean);
+
+    void deleteLabel(Integer[] ids);
+    @Select("select * from t_label where id=#{value}")
+    LabelHxyBean findByIdInfo(Integer id);
+
+    @Update("update t_label set name=#{name},sort=#{sort},img=#{img} where id=#{id}")
+    void updateLabel(LabelHxyBean labelHxyBean);
+
+    List<ldl_commodity_manage> daochu(Integer[] ids);
+
+    @Update("update ldl_commodity_manage set chezt=1 where id=#{value}")
+    void undatebyid(Integer id);
+
+    int querychecount(@Param("commodity")ldl_commodity_manage commodity);
+
+    List<ldl_commodity_manage> queryche(@Param("start")int start, @Param("rows")Integer rows, @Param("commodity")ldl_commodity_manage commodity);
+
+    @Update("update ldl_commodity_manage set chezt=0 where id=#{value}")
+    void updateOne(Integer id);
+//-----库存
+    int countPage(@Param("inventoryHxyBean")InventoryHxyBean inventoryHxyBean);
+
+    List<InventoryHxyBean> findInventoryPage(@Param("start")int start, @Param("rows")Integer rows, @Param("inventoryHxyBean")InventoryHxyBean inventoryHxyBean);
 }
--- dubbo-provider-ldl/src/main/java/com/jk/service/ldlServiceImpl.java
+++ dubbo-provider-ldl/src/main/java/com/jk/service/ldlServiceImpl.java
@@ -2,13 +2,18 @@ package com.jk.service;
 
 import com.alibaba.dubbo.config.annotation.Service;
 import com.jk.mapper.ldlMapper;
-import com.jk.pojo.ldl_commodity_manage;
-import com.jk.pojo.ldl_goods_classify;
+import com.jk.pojo.*;
+import com.jk.util.ExcelUtil;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
-import java.util.HashMap;
-import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import java.io.*;
+import java.util.*;
 
 @Service(interfaceClass=ldlService.class)
 @Component
@@ -17,17 +22,6 @@ public class ldlServiceImpl implements ldlService {
     @Autowired
     private ldlMapper ldlmapper;
 
-    //管理分类
-    @Override
-    public HashMap<String, Object> queryclassify(Integer page, Integer rows) {
-        HashMap<String, Object> hashMap = new HashMap<String, Object>();
-        int count = ldlmapper.findOrderCount(hashMap);
-        List<ldl_goods_classify> list = ldlmapper.findOrderList((page-1)*rows,rows);
-        hashMap.put("total", count);
-        hashMap.put("rows", list);
-        System.out.println(hashMap);
-        return hashMap;
-    }
 ////商品管理
     @Override
     public HashMap<String, Object> querycommodity(Integer page, Integer rows, ldl_commodity_manage commodity) {
@@ -54,7 +48,8 @@ public class ldlServiceImpl implements ldlService {
 //分类二级查询
     @Override
     public List<ldl_goods_classify> findArea(Integer id) {
-        return ldlmapper.findArea(id);
+        List<ldl_goods_classify> area = ldlmapper.findArea(id);
+        return area;
     }
 
     @Override
@@ -63,4 +58,153 @@ public class ldlServiceImpl implements ldlService {
     }
 
 
-}
+    @Override
+    public List<ldl_goods_classify> tree() {
+        return ldlmapper.tree();
+    }
+
+    //品牌查询
+    @Override
+    public HashMap<String, Object> findBrand(BrandHxyBean brandHxyBean) {
+        HashMap<String, Object> map = new HashMap<>();
+        int count=ldlmapper.countBrand(brandHxyBean);
+        int start=(brandHxyBean.getPage()-1)*brandHxyBean.getRows();
+        List<BrandHxyBean>list=ldlmapper.findBrand(start,brandHxyBean.getRows(),brandHxyBean);
+
+        map.put("total", count);
+        map.put("rows", list);
+
+        return map;
+    }
+
+    //----------------------------------------------------
+
+    @Override
+    public HashMap<String, Object> findLabel(LabelHxyBean labelHxyBean) {
+        HashMap<String, Object> hashMap = new HashMap<>();
+        int count= ldlmapper.countLabel(labelHxyBean);
+        int start = (labelHxyBean.getPage()-1)*labelHxyBean.getRows();
+        List<LabelHxyBean> list = ldlmapper.findLabel(start,labelHxyBean.getRows(),labelHxyBean);
+
+        hashMap.put("total", count);
+        hashMap.put("rows", list);
+
+        return hashMap;
+    }
+
+    @Override
+    public void saveLabel(LabelHxyBean labelHxyBean) {
+        ldlmapper.saveLabel(labelHxyBean);
+    }
+
+    @Override
+    public void deleteLabel(Integer[] ids) {
+        ldlmapper.deleteLabel(ids);
+    }
+
+    @Override
+    public LabelHxyBean findByIdInfo(Integer id) {
+
+        return ldlmapper.findByIdInfo(id);
+    }
+
+    @Override
+    public void updateLabel(LabelHxyBean labelHxyBean) {
+        ldlmapper.updateLabel(labelHxyBean);
+    }
+
+    //导出
+    @Override
+    public void daochu(Integer[] ids) {
+        List<ldl_commodity_manage>commodity= ldlmapper.daochu(ids);
+        int count = ids.length;
+        JSONArray ja = new JSONArray();
+        for(int i=0;i<commodity.size();i++){
+            JSONObject o=new JSONObject();
+            o.put("id",commodity.get(i).getId());
+            o.put("number",commodity.get(i).getNumber());
+            o.put("name",commodity.get(i).getName());
+            o.put("datetime",commodity.get(i).getDatetime());
+            o.put("state",commodity.get(i).getState());
+            o.put("goodsname",commodity.get(i).getGoodsname());
+            o.put("money",commodity.get(i).getMoney());
+            o.put("url",commodity.get(i).getUrl());
+            o.put("current",commodity.get(i).getCurrent());
+            o.put("quantity",commodity.get(i).getQuantity());
+            ja.add(o);
+        }
+        Map<String,String> headMap = new LinkedHashMap<String,String>();
+        headMap.put("id","id");
+        headMap.put("number","编号");
+        headMap.put("name","名称");
+        headMap.put("datetime","创建时间");
+        headMap.put("state","是否上架");
+        headMap.put("goodsname","商品分类");
+        headMap.put("money","价格");
+        headMap.put("url","路径");
+        headMap.put("current","库存");
+        headMap.put("quantity","入库数量");
+
+        String title = "测试";
+
+        OutputStream outXlsx=null;
+        try {
+            /*String s = UUID.randomUUID().toString();*/
+            outXlsx = new FileOutputStream("E://b.xls");
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        System.out.println(commodity);
+        System.out.println("正在导出xlsx....");
+        Date d2 = new Date();
+        ExcelUtil excelutil = new ExcelUtil();
+        excelutil.exportExcel(title,headMap,ja,null,0,outXlsx);
+        System.out.println("共"+count+"条数据,执行"+(new Date().getTime()-d2.getTime())+"ms");
+        try {
+            outXlsx.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+//购物车修改
+    @Override
+    public void joinche(Integer id) {
+                ldlmapper.undatebyid(id);
+    }
+
+    @Override
+    public HashMap<String, Object> queryche(Integer page, Integer rows, ldl_commodity_manage commodity) {
+        //查询总条数
+        int total = ldlmapper.querychecount(commodity);
+        //查询每页显示的list
+        int start = (page-1)*rows;//开始位置
+        List<ldl_commodity_manage> list = ldlmapper.queryche(start,rows,commodity);
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("total", total);
+        map.put("rows", list);
+        return map;
+    }
+    //购物车修改到展示
+    @Override
+    public void updateOne(Integer id) {
+        ldlmapper.updateOne(id);
+    }
+//-------------库存
+    @Override
+    public HashMap<String, Object> findRepertory(InventoryHxyBean inventoryHxyBean) {
+            HashMap<String, Object> hashMap = new HashMap<>();
+            int count= ldlmapper.countPage(inventoryHxyBean);
+            int start = (inventoryHxyBean.getPage()-1)*inventoryHxyBean.getRows();
+
+            List<InventoryHxyBean> list = ldlmapper.findInventoryPage(start,inventoryHxyBean.getRows(),inventoryHxyBean);
+
+            hashMap.put("total", count);
+            hashMap.put("rows", list);
+
+            return hashMap;
+        }
+    }
+
